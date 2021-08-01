778257ede8b612e3e5acc77b44f3b2801411088c
hkidocode <57941226+hkidocode@users.noreply.github.com>
2021-08-01 18:49:41 +0100

    add role repository

---
+++ src/main/java/ma/youcode/marsoul/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package ma.youcode.marsoul.repository;
+
+import ma.youcode.marsoul.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findRoleByName(String name);
+}
